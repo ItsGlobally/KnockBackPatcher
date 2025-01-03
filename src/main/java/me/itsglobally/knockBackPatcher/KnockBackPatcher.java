49dd3bfd603f6278c74f3beb5ca17b4fd5c0c51f
ItsGlobally <itsglobally1324@gmail.com>
2025-01-03 23:17:17 +0800

    ee

---
+++ src/main/java/me/itsglobally/knockBackPatcher/KnockBackPatcher.java
@@ -0,0 +1,17 @@
+package me.itsglobally.knockBackPatcher;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KnockBackPatcher extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
